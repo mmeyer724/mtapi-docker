e811b13a34d9c86faefd5ea75a6215f36eb1d4e0
Michael Meyer <michael@meyer.io>
2019-09-15 19:19:45 -0400

    Fix data types in config

--- settings_env.py
+++ settings_env.py
@@ -3,8 +3,8 @@ import os
 MTA_KEY       = os.environ.get('MTA_KEY')
 STATIONS_FILE = os.environ.get('STATIONS_FILE', './data/stations.json')
 CROSS_ORIGIN  = os.environ.get('CROSS_ORIGIN', '*')
-MAX_TRAINS    = os.environ.get('MAX_TRAINS', '10')
-MAX_MINUTES   = os.environ.get('MAX_MINUTES', '30')
-CACHE_SECONDS = os.environ.get('CACHE_SECONDS', '60')
-THREADED      = os.environ.get('THREADED', True)
-DEBUG         = os.environ.get('DEBUG', False)
+MAX_TRAINS    = int(os.environ.get('MAX_TRAINS', 10))
+MAX_MINUTES   = int(os.environ.get('MAX_MINUTES', 30))
+CACHE_SECONDS = int(os.environ.get('CACHE_SECONDS', 60))
+THREADED      = True if os.environ.get('THREADED', 'True') == 'True' else False
+DEBUG         = True if os.environ.get('DEBUG', 'False') == 'True' else False
